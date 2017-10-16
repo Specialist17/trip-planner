63b94931d517b79a279bbb866773542b1d8ff896
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-16 12:33:07 -0700

    initial structure for iOS app

--- Trip Planner/Trip Planner/Controller/TripsController.swift
+++ Trip Planner/Trip Planner/Controller/TripsController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TripsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,12 +16,6 @@ class ViewController: UIViewController {
         let basicAuthHeaders = BasicAuth.generateBasicAuthHeader(username: "fernando2@mail.com", password: "password")
         
         Networking.instance.fetch(route: Route.trips, headers: ["Authorization": basicAuthHeaders]) { (data) in
-            
-//            let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
-//            if let trips = json {
-//                print(trips)
-//                print(type(of: trips))
-//            }
             
             let trips = try? JSONDecoder().decode(Trip.self, from: data)
             guard let trip_list = trips?.trips else {return}
---
+++ Trip Planner/Trip Planner/View/TripCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripCell.swift
+//  Trip Planner
+//
+//  Created by Fernando on 10/16/17.
+//  Copyright © 2017 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class TripCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
