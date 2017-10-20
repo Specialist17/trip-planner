086eb5d094fec6638a704b18200236cc54d600f3
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-20 15:38:55 -0700

    config variable

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ import pdb
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 mongo = MongoClient(app.config['MONGODB_URI'])
-# mongo = MongoClient("mongodb://Specialist17:eQYn6B8Ve7gpsPQv33@ds127105.mlab.com:27105/trip_panner")
-# mongo = MongoClient('localhost', 27017)
 app.db = mongo.trip_panner
 app.bcrypt_rounds = 12
 api = Api(app)
